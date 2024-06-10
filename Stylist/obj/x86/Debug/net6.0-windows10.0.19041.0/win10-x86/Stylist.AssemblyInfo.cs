824eb55b86925626f45ffe27f1cf78527400d4fd
Diego Fabian Cancho Coila <u20201f479@upc.edu.pe>
2024-06-10 17:57:52 -0500

    fix: remove webui and add blazor project

--- Stylist/obj/x86/Debug/net6.0-windows10.0.19041.0/win10-x86/Stylist.AssemblyInfo.cs
+++ Stylist/obj/x86/Debug/net6.0-windows10.0.19041.0/win10-x86/Stylist.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Stylist")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7a576817ea48f2dc0a353f39969a3c63ea48335")]
 [assembly: System.Reflection.AssemblyProductAttribute("Stylist")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Stylist")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
